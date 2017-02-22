8cc049d049db608f7498f270a1fafbd618b18ff4
DAVMCKIN <DAVIDJMCKIN@GMAIL.COM>
2017-02-22 15:48:54 -0500

    initial commit

--- first_app.rb
+++ first_app.rb
@@ -1,1 +1,20 @@
+require 'bundler/setup'
 require 'sinatra'
+
+get "/" do
+  "Hello World!"
+end
+
+get "/lorem/:lipsum/?:num?" do
+  %w(standard hipster samuel).include? params[:lipsum]
+    Object.const_get(params[:lipsum].capitalize).call(params[:num])
+end
+
+get "/:lipsum" do
+  "hello #{params[:lipsum].capitalize}"
+end
+
+get "" do
+  status 400
+    post.errors.full_messages.join("\n")
+end
